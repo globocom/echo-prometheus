593020fbe4cd85f1866da99178147c04a6167282
Cristian Dean <cristian.dean@corp.globo.com>
2019-09-25 00:02:30 -0300

    Add custom config support

---
+++ example/customConfig/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+
+	echoPrometheus "github.com/globocom/echo-prometheus"
+	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func main() {
+	e := echo.New()
+
+	var configMetrics = echoPrometheus.NewConfig()
+	configMetrics.Namespace = "namespace"
+	configMetrics.Buckets = []float64{
+		0.0005, // 0.5ms
+		0.001,  // 1ms
+		0.005,  // 5ms
+		0.01,   // 10ms
+		0.05,   // 50ms
+		0.1,    // 100ms
+		0.5,    // 500ms
+		1,      // 1s
+		2,      // 2s
+	}
+
+	e.Use(echoPrometheus.MetricsMiddlewareWithConfig(configMetrics))
+	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
--- middleware.go
+++ middleware.go
@@ -1,4 +1,4 @@
-package api
+package echoprometheus
 
 import (
 	"reflect"
@@ -6,40 +6,45 @@ import (
 	echo "github.com/labstack/echo/v4"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-
 )
 
-var (
-	httpRequests = promauto.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "echo",
-		Subsystem: "http",
-		Name:      "requests_total",
-		Help:      "Number of HTTP operations",
-	}, []string{"status", "method", "handler"})
+// Config responsible to configure middleware
+type Config struct {
+	Namespace string
+	Buckets   []float64
+	Subsystem string
+}
 
-	httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: "echo",
-		Subsystem: "http",
-		Name:      "request_duration_seconds",
-		Help:      "Spend time by processing a route",
-		Buckets: []float64{
-			0.0005,
-			0.001, // 1ms
-			0.002,
-			0.005,
-			0.01, // 10ms
-			0.02,
-			0.05,
-			0.1, // 100 ms
-			0.2,
-			0.5,
-			1.0, // 1s
-			2.0,
-			5.0,
-		},
-	}, []string{"method", "handler"})
+const (
+	httpRequestsCount    = "requests_total"
+	httpRequestsDuration = "request_duration_seconds"
+	notFoundPath         = "/not-found"
 )
 
+// DefaultConfig has the default instrumentation config
+var DefaultConfig = Config{
+	Namespace: "echo",
+	Subsystem: "http",
+	Buckets: []float64{
+		0.0005,
+		0.001, // 1ms
+		0.002,
+		0.005,
+		0.01, // 10ms
+		0.02,
+		0.05,
+		0.1, // 100 ms
+		0.2,
+		0.5,
+		1.0, // 1s
+		2.0,
+		5.0,
+		10.0, // 10s
+		15.0,
+		20.0,
+		30.0,
+	},
+}
 
 func normalizeHTTPStatus(status int) string {
 	if status < 200 {
@@ -54,32 +59,60 @@ func normalizeHTTPStatus(status int) string {
 	return "5xx"
 }
 
-
 func isNotFoundHandler(handler echo.HandlerFunc) bool {
 	return reflect.ValueOf(handler).Pointer() == reflect.ValueOf(echo.NotFoundHandler).Pointer()
 }
 
-func MetricsMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		req := c.Request()
-		path := c.Path()
+// NewConfig returns a new config with default values
+func NewConfig() Config {
+	return DefaultConfig
+}
 
-		// to avoid attack high cardinality of 404
-		if isNotFoundHandler(c.Handler()) {
-			path = "/not-found"
+// MetricsMiddleware returns an echo middleware with default config for instrumentation.
+func MetricsMiddleware() echo.MiddlewareFunc {
+	return MetricsMiddlewareWithConfig(DefaultConfig)
+}
+
+// MetricsMiddlewareWithConfig returns an echo middleware for instrumentation.
+func MetricsMiddlewareWithConfig(config Config) echo.MiddlewareFunc {
+
+	httpRequests := promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: config.Namespace,
+		Subsystem: config.Subsystem,
+		Name:      httpRequestsCount,
+		Help:      "Number of HTTP operations",
+	}, []string{"status", "method", "handler"})
+
+	httpDuration := promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: config.Namespace,
+		Subsystem: config.Subsystem,
+		Name:      httpRequestsDuration,
+		Help:      "Spend time by processing a route",
+		Buckets:   config.Buckets,
+	}, []string{"method", "handler"})
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			req := c.Request()
+			path := c.Path()
+
+			// to avoid attack high cardinality of 404
+			if isNotFoundHandler(c.Handler()) {
+				path = notFoundPath
+			}
+
+			timer := prometheus.NewTimer(httpDuration.WithLabelValues(req.Method, path))
+			err := next(c)
+			timer.ObserveDuration()
+
+			if err != nil {
+				c.Error(err)
+			}
+
+			status := normalizeHTTPStatus(c.Response().Status)
+			httpRequests.WithLabelValues(status, req.Method, path).Inc()
+
+			return err
 		}
-
-		timer := prometheus.NewTimer(httpDuration.WithLabelValues(req.Method, path))
-		err := next(c)
-		timer.ObserveDuration()
-
-		if err != nil {
-			c.Error(err)
-		}
-
-		status := normalizeHTTPStatus(c.Response().Status)
-		httpRequests.WithLabelValues(status, req.Method, path).Inc()
-
-		return err
 	}
 }
